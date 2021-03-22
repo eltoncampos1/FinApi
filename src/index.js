6b832d6fa665df4519d083d6fc80d96a46e0b235
elton de campos <eltoncampos36@gmail.com>
2021-03-21 21:07:13 -0300

    [feat]: user can create account using cpf and name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const { v4: uuid } = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+const costumers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuid();
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333, () => {
   console.log("Server is running");
