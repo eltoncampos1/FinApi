52ca566379cec8be654a32f6bd3cfc862d1a133d
elton de campos <eltoncampos36@gmail.com>
2021-03-21 21:55:19 -0300

    [chore]: create a middleare to verify if existis account cpf

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Costumer not found." });
+  }
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -27,15 +39,7 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Costumer not found." });
-  }
-
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
