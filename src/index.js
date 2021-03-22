6edc2dd60b412a59faff8e1825904eefc5851726
elton de campos <eltoncampos36@gmail.com>
2021-03-21 22:22:07 -0300

    [feat]: user can make a withdrawal only if they have enough balance

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ function verifyIfExistAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalace(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -56,6 +68,27 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
     amount,
     createdAt: new Date(),
     type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalace(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "insufficient funds." });
+  }
+
+  const statementOperation = {
+    amount,
+    createdAt: new Date(),
+    type: "debit",
   };
 
   customer.statement.push(statementOperation);
