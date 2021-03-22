8588a3a2662baadf5ef2c54bbe1aadf69467a623
elton de campos <eltoncampos36@gmail.com>
2021-03-21 23:12:37 -0300

    [feat]: user can list bank statement by date

--- src/index.js
+++ src/index.js
@@ -96,6 +96,20 @@ app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/date", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.createdAt.toDateString() === new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
 app.listen(3333, () => {
   console.log("Server is running");
 });
