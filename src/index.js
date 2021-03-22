cd0560b61fcf8dd813a013274d6246c20281a121
elton de campos <eltoncampos36@gmail.com>
2021-03-21 23:27:32 -0300

    [feat]: user can delete account

--- src/index.js
+++ src/index.js
@@ -125,6 +125,14 @@ app.get("/account", verifyIfExistAccountCPF, (request, response) => {
   return response.json(customer);
 });
 
+app.delete("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
 app.listen(3333, () => {
   console.log("Server is running");
 });
